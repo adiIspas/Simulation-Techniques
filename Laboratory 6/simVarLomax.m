fc1fd20e7e473311e8f8d3cc983e0551ad6c2d8a
Adrian Ispas <adi.ispas95@gmail.com>
2016-11-11 12:20:58 +0200

    Update ...

--- Laboratory 6/simVarLomax.m
+++ Laboratory 6/simVarLomax.m
@@ -1,14 +1,6 @@
 function x = simVarLomax(lambda, a, b)
-
-%     f(x) = ( a * O ) / (O*x+1) ^ (a+1)
-%     O = lambda / b;
-%     
-%     
-%    Y = gama(0,b,a);
-   
-   % cod
-   
    y = gamrnd(a,1/b);
+   
    x = exprnd(1/(lambda * y));
 end
 
